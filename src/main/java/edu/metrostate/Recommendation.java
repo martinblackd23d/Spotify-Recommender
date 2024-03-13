6601d60d3ec699c8a8e54ea2624a3919ea907b8d
traceytreat <treatracey@gmail.com>
2024-03-13 16:39:17 -0500

    create recommendation class

---
+++ src/main/java/edu/metrostate/Recommendation.java
@@ -0,0 +1,30 @@
+package edu.metrostate;
+
+public class Recommendation {
+    private List<String> seedSongs;
+    private List<String> seedArtists;
+    private List<String> seedGenre;
+    private List<Attribute> attributes;
+
+    public Recommendation(){
+        //constructor
+    }
+    public void addSong(String song){
+        seedSongs.add(song);
+    }
+    public void addAttribute(Attribute attribute){
+        attributes.add(attribute)
+    }
+    public void addArtist(String artist){
+        seedArtists.add(artist);
+    }
+    public void addGenre(String genre){
+        seedGenre.add(genre);
+    }
+    public List<Song> getRecommendation(){
+        //makes API call
+        //this can be dummy data for now
+
+        //when this is finished it should be calling request from the Request class
+    }
+}
